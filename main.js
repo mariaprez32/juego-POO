11a22527cd5d06eef5ad0a811add299668d6180c
mariaprez32 <pereztorresmaria32@gmail.com>
2025-02-11 18:26:43 +0100

    change size and languages

--- main.js
+++ main.js
@@ -1,112 +1,152 @@
-// Clases
-
 class Game {
-  constructor(){
-    this.container = document.getElementById("game-container");
-    this.personaje = null;
-    this.monedas = [];
-    this.puntuacion = 0;
-    this.crearEscenario();
-    this.agregarEventos();
-  }
-  crearEscenario(){
-    this.personaje = new Personaje();
-    this.container.appendChild(this.personaje.element);
-    for(let i = 0; i < 5; i++){
-        const moneda = new Moneda();
-        this.monedas.push(moneda);
-        this.container.appendChild(moneda.element);
+    constructor() {
+        this.container = document.getElementById("game-container");
+        this.character = null;
+        this.pages = [];
+        this.gameWidth = this.container.offsetWidth;
+        this.gameHeight = this.container.offsetHeight;
+        this.createScenario();
+        this.addEvents();
     }
-  } 
-  agregarEventos(){
-    window.addEventListener("keydown",(e) => this.personaje.mover(e));
-    this.checkColisiones();
-  }
-  checkColisiones(){
-    setInterval(() => {
-        this.monedas.forEach((moneda,index) => {
-            if(this.personaje.colisionaCon(moneda)){
-                this.container.removeChild(moneda.element);
-                this.monedas.splice(index,1)
-            }
-        })
-    },
-        100)
-  }
-}
-class Personaje {
-    constructor(){
-        this.x = 50;
-        this.y = 300;
-        this.width = 50;
-        this.height = 50;
-        this.velocidad = 10;
-        this.saltando = false;
-        this.element = document.createElement("div");
-        this.element.classList.add("personaje");
-        this.actualizarPosicion();
-    }
-    mover(evento){
-        if(evento.key === "ArrowRight"){
-            this.x += this.velocidad;
-        }else if(evento.key === "ArrowLeft"){
-            this.x -= this.velocidad;
-        }else if(evento.key === "ArrowUp"){
-            this.saltar();
+
+    createScenario() {
+        this.character = new Character(this.gameWidth, this.gameHeight);
+        this.container.appendChild(this.character.element);
+        
+        for(let i = 0; i < 5; i++) {
+            const page = new Page(this.gameWidth, this.gameHeight);
+            this.pages.push(page);
+            this.container.appendChild(page.element);
         }
-        this.actualizarPosicion();
     }
-    saltar(){
-        this.saltando = true;
-        let alturaMaxima = this.y - 100;
-        const salto = setInterval(() => {
-            if(this.y > alturaMaxima){
-                this.y -= 10; //redondeo de la gravedad
-            }else {
-                clearInterval(salto);
-                this.caer();
+
+    addEvents() {
+        window.addEventListener("keydown", (e) => this.character.move(e));
+        window.addEventListener("resize", () => this.handleResize());
+        this.checkColisiones();
+    }
+
+    handleResize() {
+        this.gameWidth = this.container.offsetWidth;
+        this.gameHeight = this.container.offsetHeight;
+    }
+
+    checkColisiones() {
+        setInterval(() => {
+            // Iterar en reversa para evitar problemas de índice
+            for(let i = this.pages.length - 1; i >= 0; i--) {
+                if(this.character.colisionaCon(this.pages[i])) {
+                    this.container.removeChild(this.pages[i].element);
+                    this.pages.splice(i, 1);
+                }
             }
-            this.actualizarPosicion();
-        },
-            20)
+        }, 100);
     }
-    caer(){
-        const gravedad = setInterval(() => {
-            if(this.y < 300){
-                this.y += 10;
-            }else {
-                clearInterval(gravedad);
-            }
-            this.actualizarPosicion();
-        },
-            20)
+}
+
+// Aplicar herencia 
+
+class GameObject {
+    constructor(x, y, width, height, className){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.element = document.createElement("div");
+        this.element.classList.add(className);
+        this.element.style.position = "absolute";
+        this.updatePosition();
     }
-    actualizarPosicion(){
+
+    updatePosition() {
         this.element.style.left = `${this.x}px`;
         this.element.style.top = `${this.y}px`;
     }
-    colisionaCon(objeto) {
+
+    collisionWith(objeto) {
         return (
-          this.x < objeto.x + objeto.width &&
-          this.x + this.width > objeto.x &&
-          this.y < objeto.y + objeto.height &&
-          this.y + this.height > objeto.y
+            this.x < objeto.x + objeto.width &&
+            this.x + this.width > objeto.x &&
+            this.y < objeto.y + objeto.height &&
+            this.y + this.height > objeto.y
         );
     }
 }
-class Moneda {
-    constructor(){
-      this.x = Math.random() * 700 + 50; 
-      this.y = Math.random() * 250 + 50;
-      this.width = 30;
-      this.height = 30;
-      this.element = document.createElement("div");
-      this.element.classList.add("moneda");
-      this.actualizarPosicion();
+
+
+class Character extends GameObject {
+    constructor(gameWidth, gameHeight) { // Agregar parámetros
+        const initialX = (gameWidth - 50) / 2;
+        const initialY = (gameHeight - 50) / 2;
+        super(initialX, initialY, 50, 50, "character");
+        this.speed = 10;
+        this.saltando = false;
+        this.imagen = document.createElement("img");
+        this.imagen.style.width = "100%";
+        this.imagen.style.height = "100%";
+        this.imagenNormal = "./media/frontal.png";
+        this.imagenJump = "./media/salto.png";
+        this.imagen.src = this.imagenNormal;
+        this.element.appendChild(this.imagen);
     }
-    actualizarPosicion(){
-        this.element.style.left = `${this.x}px`;
-        this.element.style.top = `${this.y}px`;
+
+    move(evento) {
+        if(evento.key === "ArrowRight") {
+            this.x += this.speed;
+        } else if(evento.key === "ArrowLeft") {
+            this.x -= this.speed;
+        } else if(evento.key === "ArrowUp") {
+            this.jump();
+        }
+        this.updatePosition();
+    }
+
+    jump() {
+        if(this.jumping) return;
+        this.jumping = true;
+        this.imagen.src = this.imagenJump;
+        
+        const maxHeight = this.y - 100;
+        const saltoInterval = setInterval(() => {
+            if(this.y > maxHeight) {
+                this.y -= 10;
+            } else {
+                clearInterval(saltoInterval);
+                this.fall();
+            }
+            this.updatePosition();
+        }, 20);
+    }
+
+    fall() {
+        const gravedadInterval = setInterval(() => {
+            if(this.y < 300) {
+                this.y += 10;
+            } else {
+                clearInterval(gravedadInterval);
+                this.imagen.src = this.imagenNormal;
+                this.jumping = false;
+            }
+            this.updatePosition();
+        }, 20);
     }
 }
-const juego = new Game();
+
+class Page extends GameObject {
+    constructor(gameWidth, gameHeight) {
+        const width = 30;
+        const height = 30;
+        super(
+            Math.random() * (gameWidth - width),
+            Math.random() * (gameHeight - height),
+            width,
+            height,
+            "page"
+        );
+        this.element.style.backgroundColor = "gold";
+        this.element.style.borderRadius = "50%";
+        this.element.style.boxShadow = "0 0 10px 2px rgba(255, 223, 0, 0.8)";
+    }
+}
+
+const game = new Game();
